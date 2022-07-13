3ab1c8bf328427fd4a4303dc1ac8415238498b79
faldor20 <eli.jambu@yahoo.com>
2022-07-13 15:04:07 +0000

    working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use embedded_hal::{digital::v2::OutputPin, prelude::_embedded_hal_blocking_delay_DelayMs};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+
 use embedded_graphics::{
     mono_font::{
         ascii::{FONT_10X20, FONT_5X8, FONT_6X12, FONT_9X15},
@@ -8,106 +10,103 @@ use embedded_graphics::{
     prelude::*,
     text::Text,
 };
-use sh1106::{prelude::*, Builder};
-
-
+use esp_idf_hal::{
+    delay, gpio,
+    i2c::{self, I2c},
+    peripherals,
+    prelude::*,
+};
+//use sh1106::{interface::DisplayInterface, mode::graphics, prelude::*, Builder, displayrotation};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 fn main() {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
-    esp_idf_sys::
     println!("Hello, worldpotato!");
-
-drawText()
+    let periphs = peripherals::Peripherals::take().unwrap();
+    let pins = periphs.pins;
+    setup_i2c_display(periphs.i2c0, pins.gpio21, pins.gpio22).unwrap();
 }
 
-fn setup_i2c_display(){
-     let dp = ::Peripherals::take().unwrap();
+fn setup_i2c_display(
+    i2c: i2c::I2C0,
+    sda: gpio::Gpio21<gpio::Unknown>,
+    scl: gpio::Gpio22<gpio::Unknown>,
+) -> Result<(), anyhow::Error> {
+    let config = <i2c::config::MasterConfig as Default>::default().baudrate((400 * 1000).into());
+    let di = i2c::Master::<i2c::I2C0, _, _>::new(i2c, i2c::MasterPins { sda, scl }, config)?;
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    let mut delay = delay::Ets;
 
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
-
-    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
-
-    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
-    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
-
- let i2c = BlockingI2c::i2c1(
-        dp.I2C1,
-        (scl, sda),
-        &mut afio.mapr,
-        Mode::Fast {
-            frequency: 100.khz().into(),
-            duty_cycle: DutyCycle::Ratio2to1,
-        },
-        clocks,
-        &mut rcc.apb1,
-        1000,
-        10,
-        1000,
-        1000,
-    );
-let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
-
-display.init().unwrap();
-display.flush().unwrap();
-
-display.set_pixel(10, 20, 1);
-
-display.flush().unwrap();   
-}
-
-
-fn drawText() { 
-    let mut display: <BinaryColor> = SimulatorDisplay::new(Size::new(350, 200));
-
-    // Show smallest font with black font on white background (default value for fonts)
+    //    let display = sh1106::Builder::new().connect_i2c(di);
+    let interface = I2CDisplayInterface::new(di);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+    let mut graphics = display;
     Text::new(
         "Hello World! - default style 5x8",
         Point::new(15, 15),
         MonoTextStyle::new(&FONT_5X8, BinaryColor::On),
     )
-    .draw(&mut display)?;
+    .draw(&mut graphics)
+    .unwrap();
 
-    // Show smallest font with white font on black background
-    let style = MonoTextStyleBuilder::new()
-        .font(&FONT_5X8)
-        .text_color(BinaryColor::Off)
-        .background_color(BinaryColor::On)
-        .build();
-
-    Text::new("Hello World! - inverse 5x8", Point::new(15, 30), style).draw(&mut display)?;
-
-    // Show 6x12 Font
-    Text::new(
-        "Hello 6x12!",
-        Point::new(15, 45),
-        MonoTextStyle::new(&FONT_6X12, BinaryColor::On),
-    )
-    .draw(&mut display)?;
-
-    // Show 9x15 Font
-    Text::new(
-        "Hello 9x15!",
-        Point::new(15, 70),
-        MonoTextStyle::new(&FONT_9X15, BinaryColor::On),
-    )
-    .draw(&mut display)?;
-
-    // Show 10x20 Font
-    Text::new(
-        "Hello 10x20!",
-        Point::new(15, 95),
-        MonoTextStyle::new(&FONT_10X20, BinaryColor::On),
-    )
-    .draw(&mut display)?;
-
-    let output_settings = OutputSettingsBuilder::new().scale(2).build();
-    Window::new("Fonts", &output_settings).show_static(&display);
-
+    graphics.set_pixel(5, 5, true);
+    graphics
+        .flush()
+        .map_err(|e| anyhow::anyhow!("Display error: {:?}", e))?;
+    println!("done");
     Ok(())
 }
+
+// fn drawText<G>(display: &mut GraphicsMode<G>) -> Result<(), anyhow::Error>
+// where
+//     G: DisplayInterface,
+// {
+//     // Show smallest font with black font on white background (default value for fonts)
+//     Text::new(
+//         "Hello World! - default style 5x8",
+//         Point::new(15, 15),
+//         MonoTextStyle::new(&FONT_5X8, BinaryColor::On),
+//     )
+//     .draw(&mut display)?;
+
+//     // Show smallest font with white font on black background
+//     let style = MonoTextStyleBuilder::new()
+//         .font(&FONT_5X8)
+//         .text_color(BinaryColor::Off)
+//         .background_color(BinaryColor::On)
+//         .build();
+
+//     Text::new("Hello World! - inverse 5x8", Point::new(15, 30), style).draw(&mut display)?;
+
+//     // Show 6x12 Font
+//     Text::new(
+//         "Hello 6x12!",
+//         Point::new(15, 45),
+//         MonoTextStyle::new(&FONT_6X12, BinaryColor::On),
+//     )
+//     .draw(&mut display)?;
+
+//     // Show 9x15 Font
+//     Text::new(
+//         "Hello 9x15!",
+//         Point::new(15, 70),
+//         MonoTextStyle::new(&FONT_9X15, BinaryColor::On),
+//     )
+//     .draw(&mut display)?;
+
+//     // Show 10x20 Font
+//     Text::new(
+//         "Hello 10x20!",
+//         Point::new(15, 95),
+//         MonoTextStyle::new(&FONT_10X20, BinaryColor::On),
+//     )
+//     .draw(&mut display)?;
+
+//     // let output_settings = OutputSettingsBuilder::new().scale(2).build();
+//     // Window::new("Fonts", &output_settings).show_static(&display);
+
+//     Ok(())
+// }
